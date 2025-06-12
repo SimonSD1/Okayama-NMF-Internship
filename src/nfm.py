5bb1ce51d1eb1d56090267d305fed5b9bc131614
Simon <simonsepiolduchemin@gmail.com>
2025-06-12 09:18:58 +0900

    first implementation, bug: the distance can improve sometime

--- src/nfm.py
+++ src/nfm.py
@@ -3,35 +3,53 @@
 import numpy as np
 from typing import Tuple
 
-INITIAL_SIZE = 2
-REDUCE_SIZE=1
-
-
 def euclidian_distance(A:np.ndarray, B:np.ndarray)->float:
-    return np.sqrt(np.sum((A-B))**2)
+    return np.sum((A-B)**2)
 
 
-def nmf(V:np.ndarray, r:int, threshold:float)->Tuple[np.ndarray,np.ndarray]:
+def nmf(V:np.ndarray, r:int, iter_max:int, tolerance:float)->Tuple[np.ndarray,np.ndarray]:
     """
     Take a matrix V of size n by m and r\n
-    Return W of size n by r and H of size r by m s.t V=~WH 
-    for the given threshold on the euclidean distance
+    Return W of size n by r and H of size r by m s.t V=~WH\n
+    Stop when convergence is reached fot the given tolerance or when iter_max is reached
     """
     
     # Initialize the W and H
     (n,m)=V.shape
-    W=np.random.rand(n,r)
-    H=np.random.rand(r,m)
+    W=np.random.rand(n,r)*10
+    H=np.random.rand(r,m)*10
+
+    print("V=",V)
+    print("W=",W)
+    print("H=",H)
+    print("WH=",W@H)
+
     
-    # We apply the update rule alternating between H and V
-    
-    
-    
-    
-    
+    # We apply the update rule on H and V
+    # @ is matrix multiplication, * and / are element wise operations
+
+    previous_distance=euclidian_distance(V,W@H)
+    iter=0
+
+    while iter<iter_max:
+        H = H * ((W.transpose() @ V) / (W.transpose() @ W @ H))
+        W = W * ((V @ H.transpose()) / (W @ H @ H.transpose()))
+
+        distance = euclidian_distance(V, W@H)
+
+        print(distance)
+
+        if previous_distance-distance<tolerance:
+            break
+
+        previous_distance=distance    
+        iter+=1
+
+    return (W,H)
 
 
-A=np.random.rand(3,2)
-B=np.random.rand(3,2)
 
-euclidian_distance(A,B)
+V=np.random.rand(2,2)*10
+
+nmf(V,1,1000, 1e-50)
+
